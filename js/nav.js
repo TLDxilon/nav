f21b5413bdad265bf67fef24edcd78228cce78b1
Toya <toya.dapena@gmail.com>
2018-02-07 23:07:05 +0100

    dev

--- js/nav.js
+++ js/nav.js
@@ -1,43 +1,59 @@
-
-//var altura = $('.navbar').offset().top;
+/*
+var premenu= $(".navbar-logo--left").offset().top
 
 $(window).on('scroll', function(){
-    if ( $(window).scrollTop() > 0 ){
-        $('.navbar-logo--left').addClass('navbar-fixed');
+    if ( $(window).scrollTop() > premenu){
+        $('.navbar-logo--left, .navbar-logo--right').addClass('navbar-fixed');
     } else {
-        $('.navbar-logo--left').removeClass('navbar-fixed');
+        $('.navbar-logo--left, .navbar-logo--right').removeClass('navbar-fixed');
     }
 });
+
+/*
+
+$(window).on('scroll', function(){
+    if ( $(window).scrollTop() > altura2 ){
+        $('.navbar-logo--center .navbar-inner').addClass('navbar-fixed');
+    } else {
+        $('.navbar-logo--center .navbar-inner').removeClass('navbar-fixed');
+    }
+});
+
+*/
 $(window).on('scroll', function() {
     if ($(window).scrollTop() > 100) {
-        $('.navbar-logo--left').addClass('bg-white-nav');
+        $('.navbar-logo--left, .navbar-logo--right').addClass('bg-white-nav');
     } else  {
-        $('.navbar-logo--left').removeClass('bg-white-nav');
+        $('.navbar-logo--left, .navbar-logo--right').removeClass('bg-white-nav');
     }
 
 
 });
-
+//subo el menú cuando llego a 600 de scroll
 $(window).on('scroll', function() {
     if ($(window).scrollTop() > 600) {
-        $('.navbar-logo--left').addClass('is-hide');
+        $('.navbar-logo--left, .navbar-logo--right').addClass('is-hide');
     } else  {
 
     }
-
-
 });
+
+//bajo el menú cuando subo el scroll
 var lastScrollPosition = 0;
 window.onscroll = function() {
     var newScrollPosition = window.scrollY;
 
     if (newScrollPosition < lastScrollPosition){
-        $('.navbar-logo--left').removeClass('is-hide');
+        $('.navbar-logo--left, .navbar-logo--right').removeClass('is-hide');
     }else{
 
     }
     lastScrollPosition = newScrollPosition;
 }
+
+
+
+
 
 $(document).ready(function(){
     $('.page-is-static .btn-ham').click(function(){
