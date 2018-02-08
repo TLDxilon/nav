c9b3944323095446bfd9c69fec3154d9ba742a90
toya <toya.dapena@gmail.com>
2018-02-08 09:57:58 +0100

    dev

--- _site/js/nav.js
+++ _site/js/nav.js
@@ -1,59 +1,45 @@
+
+//var altura = $('.navbar').offset().top;
 /*
-var premenu= $(".navbar-logo--left").offset().top
-
 $(window).on('scroll', function(){
-    if ( $(window).scrollTop() > premenu){
-        $('.navbar-logo--left, .navbar-logo--right').addClass('navbar-fixed');
+    if ( $(window).scrollTop() > 0 ){
+        $('.navbar-logo--left').addClass('navbar-fixed');
     } else {
-        $('.navbar-logo--left, .navbar-logo--right').removeClass('navbar-fixed');
+        $('.navbar-logo--left').removeClass('navbar-fixed');
     }
-});
+});*/
 
-/*
 
-$(window).on('scroll', function(){
-    if ( $(window).scrollTop() > altura2 ){
-        $('.navbar-logo--center .navbar-inner').addClass('navbar-fixed');
-    } else {
-        $('.navbar-logo--center .navbar-inner').removeClass('navbar-fixed');
-    }
-});
-
-*/
 $(window).on('scroll', function() {
-    if ($(window).scrollTop() > 100) {
-        $('.navbar-logo--left, .navbar-logo--right').addClass('bg-white-nav');
+    if ($(window).scrollTop() > 60) {
+        $('.navbar-logo--left').addClass('bg-white-nav');
     } else  {
-        $('.navbar-logo--left, .navbar-logo--right').removeClass('bg-white-nav');
+        $('.navbar-logo--left').removeClass('bg-white-nav');
     }
 
 
 });
-//subo el menú cuando llego a 600 de scroll
+
 $(window).on('scroll', function() {
     if ($(window).scrollTop() > 600) {
-        $('.navbar-logo--left, .navbar-logo--right').addClass('is-hide');
+        $('.navbar-logo--left').addClass('is-hide');
     } else  {
 
     }
-});
 
-//bajo el menú cuando subo el scroll
+
+});
 var lastScrollPosition = 0;
 window.onscroll = function() {
     var newScrollPosition = window.scrollY;
 
     if (newScrollPosition < lastScrollPosition){
-        $('.navbar-logo--left, .navbar-logo--right').removeClass('is-hide');
+        $('.navbar-logo--left').removeClass('is-hide');
     }else{
 
     }
     lastScrollPosition = newScrollPosition;
 }
-
-
-
-
 
 $(document).ready(function(){
     $('.page-is-static .btn-ham').click(function(){
@@ -68,5 +54,14 @@ $(document).ready(function(){
         $('.header, .content, .footer').addClass('page-is-static');
         $('.menu-movil').removeClass('display-block');
         $('body').removeClass('overflow-hidden');
+    });
+    $('.btn-search').click(function(){
+    $( ".search-bar" ).slideToggle(
+        function() {
+            $(this).addClass( "selected" );
+        }, function() {
+            $( this ).removeClass( "selected" );
+        }
+    );
     });
 })
